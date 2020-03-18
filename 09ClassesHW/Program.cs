1f27f2d0af4b89e774879e687c0a2d4b3bf6c6a0
Grek <grrekov@yandex.ru>
2020-03-18 16:38:51 +0300

    Добавьте файлы проекта.

---
+++ 09ClassesHW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09ClassesHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aver myDelegat = new Aver(delegate (int a, int b, int c) { Console.WriteLine((a + b + c) / 3); });
+            int x = 5;
+            int y = 5;
+            int z = 5;
+            myDelegat.Invoke(x, y, z);
+        }
+    }
+    public delegate void Aver(int a, int b, int c);
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum sum = (a, b) => a + b;
+            Sub sub = (a, b) => a - b;
+            Mul mul = (a, b) => a * b;
+            Div div = (a,b) => { if (b != 0) return a / b; else { Console.WriteLine("Не ну как так? Сейчас мы тебя на ноль поделим. Приятно будет?"); return 0; } };
+            int x = 5;
+            int y = 5;
+            Console.WriteLine(div.Invoke(x, y));
+            Console.WriteLine(sum(x, y));
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public delegate int Sum(int a, int b);
+    public delegate int Sub(int a, int b);
+    public delegate int Mul(int a, int b);
+    public delegate int Div(int a, int b);
+}
